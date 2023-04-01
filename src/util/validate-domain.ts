d7eb14e902134538b69898d552ea11532bba6b94
wmxpy <wm@mengw.io>
2023-04-01 00:31:39 -0500

    implement validate domain method

---
+++ src/util/validate-domain.ts
@@ -0,0 +1,13 @@
+/**
+ * @author WMXPY
+ * @namespace Util
+ * @description Validate Domain
+ */
+
+export const validateDomain = (domain: string): boolean => {
+
+    const regex: RegExp = new RegExp(
+        '^[a-z0-9-]+\.([a-z0-9-]+\.)*[a-z0-9-]+$',
+    );
+    return regex.test(domain);
+};
